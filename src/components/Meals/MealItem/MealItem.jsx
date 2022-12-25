da14ca678c838dbf458a91eb14210b088ce9e2c2
Kadir <k1d12r04@gmail.com>
2022-12-25 14:54:19 +0300

    Done meal list and meal item info

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,7 +1,23 @@
+import { Fragment } from 'react';
 import styles from './MealItem.module.scss';
 
-const MealItem = () => {
-  return <h3>Yemek Listesi</h3>;
+const MealItem = props => {
+  return (
+    <div className={styles['meal-item']}>
+      <div className={styles['meal-info']}>
+        <h3 className={styles['meal-info__name']}>{props.name}</h3>
+        <p className={styles['meal-info__desc']}> {props.description} </p>
+        <p className={styles['meal-info__price']}> ${props.price} </p>
+      </div>
+      <div className={styles['add-meal']}>
+        <div className={styles['add-meal__info']}>
+          <h3>Amount</h3>
+          <p>INPUT</p>
+        </div>
+        <h3>ADD BUTTON</h3>
+      </div>
+    </div>
+  );
 };
 
 export default MealItem;
